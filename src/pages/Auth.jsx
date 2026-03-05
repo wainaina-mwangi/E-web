29a59072e9105f60f7e6df8cebd92216d10acdd8
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-03-05 14:23:30 +0300

    implemented a state change on a form and addedd the new form

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,34 @@
+
 import React from "react";
+import { useState } from "react";
 
 function Auth() {
+  const[mode,setMode]=useState('SignUp');
   return (
     <div className="page">
       <div className="container">
         <div className="auth-container">
-          <h1 className="page-title"></h1>
+          <h1 className="page-title">{mode === 'SignUp' ? 'SignUp':'Login'}</h1>
+          <form className="auth-form">
+            <div className="form-group">
+              <label htmlFor="email" className="form-label">Email </label>
+              <input type="email"  className="form-input" id='email'/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="form-label">Password </label>
+              <input type="password"  className="form-input" id="password" />
+            </div>
+            <button className="btn btn-secondary" type="submit">{mode === 'SignUp' ? 'SignUp':'Login'}</button>
+          </form>
+          <div className="auth-switch">
+           {mode === 'SignUp' ? 
+            (<p>Already have an account ? <span className="auth-link" onClick={()=>setMode('Login')}>Login</span> </p> ):(
+
+              <p>dont have an account ? <span className="auth-link" onClick={()=>setMode('SignUp')}>SignUp</span> </p>
+            )
+           
+           }
+          </div>
         </div>
       </div>
     </div>
