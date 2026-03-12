642f26835590b9811f543c515b9079ad17dcd4e4
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-03-13 01:49:51 +0300

    finalizing the checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useCart } from "../Context/cartContext";
 
 function Checkout() {
+  const {}=useCart();
   return (
-    <div>Checkout</div>
+    <div className='page'>
+      <div className='container'>
+     <h1 className='page-title'>checkout</h1>
+     <div className='checkout-container'>
+      <div className='checkout-items'>
+        <h1>order summary</h1>
+      </div>
+     </div>
+      </div>
+    </div>
   )
 }
 
